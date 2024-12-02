9d62180dd2782ce3ab235afedaae780a1e69134e
Maximilian Schwaerzler <maschwaerzler@a1.net>
2024-12-02 20:29:40 +0100

    Add the GameOfLife class

---
+++ src/main/java/at/co/schwaerzler/maximilian/GameOfLife.java
@@ -0,0 +1,14 @@
+package at.co.schwaerzler.maximilian;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameOfLife {
+    static final int GAME_FIELD_WIDTH = 50;
+    static final int GAME_FIELD_HEIGHT = 50;
+    private Set<Cell> aliveCells = new HashSet<>();
+
+    public GameOfLife() {
+
+    }
+}

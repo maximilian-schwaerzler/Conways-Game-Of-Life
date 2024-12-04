1e250f9c3bb338c3f6bcb932ba81d19a45eb2530
Maximilian Schwaerzler <maschwaerzler@a1.net>
2024-12-04 12:51:54 +0100

    Add a lot of stuff :)

--- src/main/java/at/co/schwaerzler/maximilian/GameFrame.java
+++ src/main/java/at/co/schwaerzler/maximilian/GameFrame.java
@@ -10,26 +10,57 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GameFrame extends JFrame implements MouseListener, KeyListener {
+    public static final int DEFAULT_GAME_WIDTH = 50;
+    public static final int DEFAULT_GAME_HEIGHT = 50;
+
+    private GameOfLife game;
+    private GamePanel gp;
+
     public GameFrame() throws HeadlessException {
         super();
 
         if (GraphicsEnvironment.isHeadless()) {
-//            System.err.println("In headless environment. This is a game, what were you thinking?");
             throw new HeadlessException("In headless environment. This is a game, what were you thinking?");
         }
 
-        setSize(500, 500);
-        setLayout(null);
+
+        // setSize(500, 500);
+        gp = new GamePanel(500, 500);
+        setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
         setTitle("Conway's Game of Life");
         addMouseListener(this);
         addKeyListener(this);
+        setResizable(false);
+
+        // setMinimumSize(new Dimension(200, 200));
+
+        getContentPane().add(gp);
+        pack();
+        game = new GameOfLife();
     }
+
+    public Cell getCellFromPoint(Point p) {
+        int cellXPos = (getWidth() / DEFAULT_GAME_WIDTH) * p.x;
+        int cellYPos = getHeight() / DEFAULT_GAME_HEIGHT * p.y;
+        return new Cell(cellXPos, cellYPos);
+    }
+
+    // Update the window with the game info
+    public void updateWindow() {
+        for (Cell cell : game.getAliveCells()) {
+            // TODO
+        }
+    }
+
+    /*
+    MOUSE LISTENERS
+     */
 
     @Override
     public void mouseClicked(@NotNull MouseEvent e) {
-        System.out.println(e.getPoint());
+        Cell clickedCell = getCellFromPoint(e.getPoint());
+        game.toggleCell(clickedCell);
     }
 
     @Override
@@ -51,6 +82,10 @@ public class GameFrame extends JFrame implements MouseListener, KeyListener {
     public void mouseExited(MouseEvent e) {
 
     }
+
+    /*
+    KEY LISTENERS
+     */
 
     @Override
     public void keyTyped(KeyEvent e) {

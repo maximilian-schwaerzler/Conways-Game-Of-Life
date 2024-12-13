948ef6fd048ee53e74b16b3d49b7bc6d1f7733a5
Maximilian Schwaerzler <maschwaerzler@a1.net>
2024-12-13 19:42:04 +0100

    Implement get bounding box

--- src/main/java/at/co/schwaerzler/maximilian/GameState.java
+++ src/main/java/at/co/schwaerzler/maximilian/GameState.java
@@ -1,10 +1,11 @@
 package at.co.schwaerzler.maximilian;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.HashSet;
 
 /**
- * A custom hash set for holding the game state
+ * A custom hash set of cells for holding the game state
  */
 public class GameState extends HashSet<Cell> {
     public GameState offset(int deltaX, int deltaY) {
@@ -17,7 +18,13 @@ public class GameState extends HashSet<Cell> {
     }
 
     public Rectangle getBoundingBox() {
-        // TODO
-        return new Rectangle();
+        int[] xCoords = this.stream().mapToInt(Cell::x).toArray();
+        int[] yCoords = this.stream().mapToInt(Cell::y).toArray();
+        int minX = Arrays.stream(xCoords).min().orElse(0);
+        int maxX = Arrays.stream(xCoords).max().orElse(0);
+        int minY = Arrays.stream(yCoords).min().orElse(0);
+        int maxY = Arrays.stream(yCoords).max().orElse(0);
+
+        return new Rectangle(minX, minY, maxX, maxY);
     }
 }

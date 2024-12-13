5358af925e0d8e4be470ac9c90f274c03e8384a7
Maximilian Schwaerzler <maschwaerzler@a1.net>
2024-12-13 20:31:20 +0100

    Add suport for Log4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,14 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.jetbrains:annotations:24.0.0")
+
     implementation("commons-cli:commons-cli:1.9.0")
     implementation("commons-io:commons-io:2.18.0")
+
+    implementation(platform("org.apache.logging.log4j:log4j-bom:3.0.0-beta3"))
+    runtimeOnly("org.apache.logging.log4j:log4j-core")
+    runtimeOnly("org.apache.logging.log4j:log4j-layout-template-json")
+    implementation("org.apache.logging.log4j:log4j-api")
 }
 
 tasks.test {
